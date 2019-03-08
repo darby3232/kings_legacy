c8e5b542b54d42ec4135e4f6ce09a7773881c353
Darby <danderson33@wisc.edu>
2019-03-08 11:43:54 -0600

    Added some basic and incomplete Lord and Land scripts.

---
+++ Assets/Scripts/Lord.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lord : MonoBehaviour
+{
+
+    public Color LordsColor;
+
+    private bool isKing;
+    private float specialLandChance; 
+    
+    private int numberArmies;
+    private int numberLands; //replace with list when implementing special Lands
+    private int numberWealth;
+
+
+    public Lord(bool isKing, Color lordsColor, float specialLandChance)
+    {
+        this.isKing = isKing;
+        LordsColor = lordsColor;
+        this.specialLandChance = specialLandChance; 
+    }
+
+    public int GetWealth()
+    {
+        return numberWealth; 
+    }
+
+    public void GainArmies(int numWealthSpent)
+    {
+        if(numWealthSpent <= numberWealth)
+        {
+            numberArmies += numWealthSpent; 
+        }
+    }
+
+
+    public int GetArmies()
+    {
+        return numberArmies; 
+    }
+
+    public int GetLands()
+    {
+        return numberLands;
+    }
+}
